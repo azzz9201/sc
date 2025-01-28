9ee79b3ab00c852844cec1129c3fbfab090d4914
azzz9201 <azizsukaepep@gmail.com>
2025-01-28 16:19:30 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 echo "Proses Update"
-apt update
-echo "Update Selesai.."
-echo "Proses Upgrade"
-apt upgrade
-echo "Upgrade Selesai.."
+bash <(curl -s https://pterodactyl-installer.se)
